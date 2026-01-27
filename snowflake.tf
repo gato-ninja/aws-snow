62928ff32459e1c33c391e5d3a3272745f08f458
Mateus Sales Albino <mateus3009@gmail.com>
2026-01-26 22:25:47 -0300

    fix

--- snowflake.tf
+++ snowflake.tf
@@ -1,10 +1,10 @@
 provider "snowflake" {
-  organization_name          = "BCABAJN"
-  account_name               = "JG57739"
-  user                       = "TERRAFORM"
+  organization_name = "BCABAJN"
+  account_name      = "JG57739"
+  user              = "TERRAFORM"
   # authenticator              = "WORKLOAD_IDENTITY"
   # workload_identity_provider = "OIDC"
-  # role                       = "ACCOUNTADMIN"
+  role = "ACCOUNTADMIN"
 }
 
 # SNOWFLAKE_TOKEN
