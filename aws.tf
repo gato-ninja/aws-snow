3af67cbbf588d86acc1781d8f6f9b2039e1cd57a
Mateus Sales Albino <mateus3009@gmail.com>
2026-01-26 22:03:05 -0300

    comment providers

--- aws.tf
+++ aws.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "us-east-2"
 
-  assume_role {
-    role_arn     = "arn:aws:iam::759817713039:role/github_actions_adm"
-    session_name = "github-actions"
-  }
+  # assume_role {
+  #   role_arn     = "arn:aws:iam::759817713039:role/github_actions_adm"
+  #   session_name = "github-actions"
+  # }
 }
 
 data "aws_caller_identity" "current" {}
--- snowflake.tf
+++ snowflake.tf
@@ -1,10 +1,10 @@
 provider "snowflake" {
-  organization_name          = "BCABAJN"
-  account_name               = "JG57739"
-  user                       = "TERRAFORM"
-  authenticator              = "WORKLOAD_IDENTITY"
-  workload_identity_provider = "OIDC"
-  role                       = "ACCOUNTADMIN"
+  # organization_name          = "BCABAJN"
+  # account_name               = "JG57739"
+  # user                       = "TERRAFORM"
+  # authenticator              = "WORKLOAD_IDENTITY"
+  # workload_identity_provider = "OIDC"
+  # role                       = "ACCOUNTADMIN"
 }
 
 # SNOWFLAKE_TOKEN
