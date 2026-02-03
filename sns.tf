01d4dd1dea74c2eb2c47cff90317c4fd0e89e9a6
Mateus Sales Albino <mateus3009@gmail.com>
2026-02-03 08:39:15 -0300

    pipe

--- sns.tf
+++ sns.tf
@@ -4,8 +4,14 @@ resource "aws_sns_topic" "s3_notifications" {
   name = "snowflake-s3-notifications"
 }
 
+# Get the Snowflake IAM policy for SNS access
+data "snowflake_system_get_aws_sns_iam_policy" "snowflake_policy" {
+  aws_sns_topic_arn = aws_sns_topic.s3_notifications.arn
+}
+
 # SNS Topic Policy Document
 data "aws_iam_policy_document" "s3_notifications_policy" {
+  # Allow S3 to publish notifications
   statement {
     effect = "Allow"
     principals {
@@ -27,17 +33,15 @@ data "aws_iam_policy_document" "s3_notifications_policy" {
       values   = [aws_s3_bucket.simple_bucket.arn]
     }
   }
+
+  # Merge Snowflake's required SNS policy
+  source_policy_documents = [
+    data.snowflake_system_get_aws_sns_iam_policy.snowflake_policy.aws_sns_topic_policy_json
+  ]
 }
 
-# SNS Topic Policy to allow S3 to publish
+# SNS Topic Policy to allow S3 to publish and Snowflake to subscribe
 resource "aws_sns_topic_policy" "s3_notifications_policy" {
   arn    = aws_sns_topic.s3_notifications.arn
   policy = data.aws_iam_policy_document.s3_notifications_policy.json
-}
-
-# SNS Subscription to Snowflake SQS queue
-resource "aws_sns_topic_subscription" "snowflake_pipe_subscription" {
-  topic_arn = aws_sns_topic.s3_notifications.arn
-  protocol  = "sqs"
-  endpoint  = snowflake_pipe.pipe.notification_channel
 }
